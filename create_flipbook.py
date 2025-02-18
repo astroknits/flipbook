f8e8c7bce315cd9990793d037ab120e143cfbaa1
Laura Kasian <laura.kasian@gmail.com>
2025-02-18 13:23:21 -0800

    update main function

--- create_flipbook.py
+++ create_flipbook.py
@@ -1,29 +1,21 @@
-from PIL import Image
-import PyPDF2
 from parse_args import parse_args
 from flipbook import Flipbook
-
-
 
 def main():
     # Parse command line arguments
     args = parse_args()
 
-    # Get the vide file name from the command line arguments
-    filename = args.filename
+    # Instantiate flipbook object
+    flipbook = Flipbook(
+        args.filename,
+        args.output_dir,
+        output_base_name=args.output_base_name,
+        output_frame_rate=args.output_frame_rate)
 
-    # Get output directory from command line args
-    output_dir = args.output_dir
-
-    # Get base name for output PDF files
-    output_base_name = args.output_base_name
-
-    # Output frame rate
-    output_frame_rate = args.output_frame_rate
-
-    flipbook = Flipbook(filename, output_dir, output_base_name=output_base_name, output_frame_rate=output_frame_rate)
+    # Print the info
     flipbook.print_info()
 
+    # Create the flipbook
     flipbook.run()
 
 
