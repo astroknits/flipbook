0c480eebc7e519950b6fb2eaf5e76da2b0896d7d
Laura Kasian <laura.kasian@gmail.com>
2025-02-21 08:44:07 -0800

    use PdfWrite to combine PDF pages

--- src/flipbook.py
+++ src/flipbook.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from math import ceil
 from tqdm import tqdm
@@ -5,7 +6,7 @@ import cv2
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-from pypdf import PdfMerger
+from pypdf import PdfWriter
 
 from src.flipbook_constants import FlipbookConstants
 from src.input_format import InputFormat
@@ -22,7 +23,6 @@ class Flipbook:
             self,
             filename,
             output_dir,
-            output_base_name=None,
             ncols=3,
             nrows=3,
             output_frame_rate=0,
@@ -35,10 +35,6 @@ class Flipbook:
 
         # output directory for output for flipbook
         self.output_dir = output_dir
-
-        # base name for output files for flipbook
-        # (default -> same base as the input video)
-        self.output_base_name = self.validate_base_name(output_base_name)
 
         self.output_format = OutputFormat(
                                 nrows,
@@ -64,20 +60,42 @@ class Flipbook:
         print('----------------------------\n\n')
 
 
-    def validate_base_name(self, output_base_name):
-        if output_base_name is not None:
-            return output_base_name
-        return Path(self.input_video.filename).stem
+    def pad(self):
+        return self.output_format.frame_border_padding
 
-    def create_data_dir(self):
+    def left_pad(self):
+        return self.output_format.left_binding_padding
+
+    def frame_width(self):
+        return self.input_video.width
+
+    def frame_height(self):
+        return self.input_video.height
+
+    def ncols(self):
+        return self.output_format.ncols
+
+    def nrows(self):
+        return self.output_format.nrows
+
+    def grid_width(self):
+        return int((self.frame_width() + 2 * self.pad() + self.left_pad()) * self.ncols())
+
+    def grid_height(self):
+        return int((self.frame_height() + 2 * self.pad()) * self.nrows())
+
+    def create_output_dir(self):
         output_dir_path = Path(self.output_dir)
         if Path.exists(output_dir_path):
             if output_dir_path.is_dir():
+                if os.listdir(self.output_dir):
+                    raise Exception((f'Output directory {self.output_dir} '
+                                     'already exists and is nonempty.'))
                 return True
-            raise Exception(f'Expected data output directory path {self.output_dir} exists but is not a directory.')
+            raise Exception((f'Expected data output directory path {self.output_dir}'
+                             ' exists but is not a directory.'))
         # If the data_dir does not exist, crate it
         output_dir_path.mkdir()
-        del output_dir_path
         return True
 
     def extract_frames(self):
@@ -89,11 +107,11 @@ class Flipbook:
         cam = cv2.VideoCapture(self.input_video.filename)
 
         # Create a directory for the extracted frames
-        self.create_data_dir()
+        self.create_output_dir()
 
         # Cycle through the frames
         index_out = 0
-        self.frames = []
+        frames = []
         for index_in in range(self.input_video.total_frames):
             # Read the frame
             success, frame = cam.read()
@@ -112,15 +130,26 @@ class Flipbook:
                     self.input_video.total_frames = index_in
                     break
                 # otherwise we process the frame
-                self.frames.append(frame)
+                frames.append(frame)
                 index_out += 1
 
         self.n_flipbook_frames = index_out
         cam.release()
         cv2.destroyAllWindows()
+        return frames
 
-    def get_output_name(self, batch_no):
-        return Path(self.output_dir).joinpath(Path(f'{self.output_base_name}.{str(batch_no)}.pdf'))
+    def get_base_output_name(self):
+        '''
+        Base file name is based on input file name
+        '''
+        return Path(self.input_video.filename).stem
+
+    def get_output_name(self, batch_no=None):
+        if batch_no is None:
+            filename = f'{self.get_base_output_name()}.pdf'
+        else:
+            filename = f'{self.get_base_output_name()}.{str(batch_no)}.pdf'
+        return Path(self.output_dir).joinpath(Path(filename))
 
     def add_watermark_to_img(self, img, watermark_text):
         # https://www.tutorialspoint.com/python_pillow/python_pillow_creating_a_watermark.htm
@@ -165,31 +194,7 @@ class Flipbook:
 
         grid.save(batch_filename)
 
-    def pad(self):
-        return self.output_format.frame_border_padding
-
-    def left_pad(self):
-        return self.output_format.left_binding_padding
-
-    def frame_width(self):
-        return self.input_video.width
-
-    def frame_height(self):
-        return self.input_video.height
-
-    def ncols(self):
-        return self.output_format.ncols
-
-    def nrows(self):
-        return self.output_format.nrows
-
-    def grid_width(self):
-        return int((self.frame_width() + 2 * self.pad() + self.left_pad()) * self.ncols())
-
-    def grid_height(self):
-        return int((self.frame_height() + 2 * self.pad()) * self.nrows())
-
-    def write_output(self):
+    def write_output_pdfs(self, frames):
         # total number of images per page
         num_per_page = self.output_format.nrows * self.output_format.ncols
 
@@ -203,16 +208,31 @@ class Flipbook:
                 desc=f'Writing output to {self.output_dir}/'
             ):
             # Get subset of frames for the batch
-            frames_in_batch = self.frames[batch_no * num_per_page: (batch_no + 1) * num_per_page]
+            frames_in_batch = frames[batch_no * num_per_page: (batch_no + 1) * num_per_page]
             self.write_tiled_batch(frames_in_batch, batch_no)
 
-        del self.frames
-        filenames = '\n'.join([str(self.get_output_name(batch_no)) for batch_no in range(num_pages_to_print)])
-        print(f'\n\nWrote the following pages:\n{filenames}')
+        print()
+        return [self.get_output_name(batch_no) for batch_no in range(num_pages_to_print)]
+
+    def combine_pdfs(self, output_frames):
+        output_file_name = self.get_output_name(None)
+        merger = PdfWriter()
+        for page in output_frames:
+            merger.append(page)
+
+        merger.write(output_file_name)
+        merger.close()
+        for page in output_frames:
+            os.remove(page)
+        print(f'\nWrote {output_file_name}\n\n')
+
+    def write_output(self, frames):
+        output_frames = self.write_output_pdfs(frames)
+        self.combine_pdfs(output_frames)
 
     def run(self):
         # Read the video and extract the frames
-        self.extract_frames()
+        frames = self.extract_frames()
 
         # Write the PDFs to output files
-        self.write_output()
+        self.write_output(frames)
