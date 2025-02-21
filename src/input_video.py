8ad9dd04d475c000f30b222eb264ef84a92c0a53
Laura Kasian <laura.kasian@gmail.com>
2025-02-21 04:07:56 -0800

    clean up input_video.py

--- src/input_video.py
+++ src/input_video.py
@@ -10,17 +10,32 @@ class InputVideo:
     '''
     def __init__(self, filename):
         '''
-        Validate existence of input media file and check format.
-        Also gather metadata
-        :param filename:
+        Validate existence of input video file and check format.
+        Also gather metadata from input video
         '''
         self.filename = self.validate_video_file(filename)
-        self.get_video_metadata()
 
-    def print(self):
-        print(f'Input file: {self.filename}')
-        print(f'Resolution: {self.get_resolution()}')
-        print(f'Frame rate: {self.frame_rate:.2f} fps')
+        '''
+        Read the video properties
+        See opencv docs page for more info
+        https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html
+        '''
+        cam = cv2.VideoCapture(self.filename)
+
+        # Video frame rate in frames per second
+        self.frame_rate = cam.get(cv2.CAP_PROP_FPS)
+
+        # Video width in pixels
+        self.width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+        # Height of the frames in the video stream (in pixels)
+        self.height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+        # Total number of frames in the video file
+        self.total_frames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        cam.release()
+        cv2.destroyAllWindows()
 
     def validate_video_file(self, filename):
         '''
@@ -38,16 +53,14 @@ class InputVideo:
         return filename
 
     def get_resolution(self):
+        '''
+        Simple helper function to return wxh resolution
+        '''
         return f'{self.width}x{self.height}'
 
-    def get_video_metadata(self):
-        # Open the file and open stream
-        cam = cv2.VideoCapture(self.filename)
+    def print(self):
+        print(f'Input file: {self.filename}')
+        print(f'Resolution: {self.get_resolution()}')
+        print(f'Frame rate: {self.frame_rate:.2f} fps')
 
-        self.frame_rate = cam.get(cv2.CAP_PROP_FPS)
-        self.width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        self.total_frames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        cam.release()
-        cv2.destroyAllWindows()
